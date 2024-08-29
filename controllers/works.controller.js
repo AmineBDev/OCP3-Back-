19538bfb4b798ae79bfa80437231aaffde12f50d
Amine Bensaoucha <bensaoucha84@gmail.com>
2024-08-29 04:26:04 +0200

    correction du lien

--- controllers/works.controller.js
+++ controllers/works.controller.js
@@ -1,18 +1,18 @@
 const db = require('./../models');
 const Works = db.works
 
-exports.findAll = async (req, res) =>  {
-	const works = await Works.findAll({include: 'category'});
+exports.findAll = async (req, res) => {
+	const works = await Works.findAll({ include: 'category' });
 	return res.status(200).json(works);
 }
 
 exports.create = async (req, res) => {
-	const host = req.get('host');
+	// const host = req.get('host');
 	const title = req.body.title;
 	const categoryId = req.body.category;
 	const userId = req.auth.userId;
-	const imageUrl = `${req.protocol}://${host}/images/${req.file.filename}`;
-	try{
+	const imageUrl = `${req.protocol}://ocp3-back.onrender.com/images/${req.file.filename}`;
+	try {
 		const work = await Works.create({
 			title,
 			imageUrl,
@@ -20,17 +20,17 @@ exports.create = async (req, res) => {
 			userId
 		})
 		return res.status(201).json(work)
-	}catch (err) {
+	} catch (err) {
 		return res.status(500).json({ error: new Error('Something went wrong') })
 	}
 }
 
 exports.delete = async (req, res) => {
-	try{
-		await Works.destroy({where:{id: req.params.id}})
-		return res.status(204).json({message: 'Work Deleted Successfully'})
-	}catch(e){
-		return res.status(500).json({error: new Error('Something went wrong')})
+	try {
+		await Works.destroy({ where: { id: req.params.id } })
+		return res.status(204).json({ message: 'Work Deleted Successfully' })
+	} catch (e) {
+		return res.status(500).json({ error: new Error('Something went wrong') })
 	}
 
 }
